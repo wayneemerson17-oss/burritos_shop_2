d93be9dde7d144d0de85fb59cb87ebdfad8c808b
wayneemerson17-oss <wayneemerson17@gmal.com>
2026-02-10 13:13:04 -0600

    updated burrito

--- burritos_shop_2/Program.cs
+++ burritos_shop_2/Program.cs
@@ -84,10 +84,37 @@ class Program
         Console.WriteLine("------------------------------------");
 
         // Please Enter Your For Loop Codes Here
+        decimal unitprice = 9.00m;
+        decimal mincost = 1200.00m;
+        decimal unitcost = 3.80m;
+        int itemsSold = 0;
+        decimal revenue = 0.00m;
+        decimal cost = 0.00m;
+        decimal profit = 0.00m;
+        for (itemsSold = 0; ; itemsSold++)
+        {
+             revenue = itemsSold * unitprice;
+             cost = mincost + (itemsSold * unitcost);
+            profit = revenue - cost;
+            if (profit <= 0)
+            {
+                Console.WriteLine($"After selling {itemsSold} burritos: profit {profit:F2}.");
+               
+            }
+            else
+            {
+                break;
+            }
+           
+        }
 
 
-
-
+        Console.WriteLine("------------------------------------");
+        Console.WriteLine("Success! Profit reached.");
+        Console.WriteLine($"Number of Burritos: {itemsSold}");
+        Console.WriteLine($"Revenue: ${revenue:F2}");
+        Console.WriteLine($"Cost: ${cost:F2}");
+        Console.WriteLine($"Profit: ${profit:F2}");
 
         Console.WriteLine("\nPress any key to return to menu...");
         Console.ReadKey();
